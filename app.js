da6b66cb084cbe0a5a85f19c30faef4bd1b495ce
Grant Collins <grantcollins03@gmail.com>
2023-04-03 20:51:46 -0400

    displayBooks()

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-let library = [];
+let library = [
+    {
+        title: 'Dune',
+        author: 'Frank Herbert',
+        pages: 412,
+        year: 1965,
+        read: 'Yes'
+    },
+    {
+        title: 'Foundation',
+        author: 'Isaac Asimov',
+        pages: 255,
+        year: 1951,
+        read: 'Yes'
+    },
+    {
+        title: 'The Third-Body Problem',
+        author: 'Liu Cixin',
+        pages: 302,
+        year: 2014,
+        read: 'No'
+    },
+    {
+        title: 'Project Hail Mary',
+        author: 'Andy Weir',
+        pages: 496,
+        year: 2021,
+        read: 'Yes'
+    },
+
+];
 
 class Book {
-    constructor(author, title, pages, read) {
+    constructor(title, author, pages, year, read) {
 
     }
 };
@@ -9,3 +39,21 @@ class Book {
 function addBookToLibrary(book) {
     return library.push(book);
 };
+
+const libGrid = document.getElementById('lib-grid');
+
+function displayBooks(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let newBook = document.createElement('div');
+        newBook.classList.add('book')
+        newBook.innerHTML = `
+        <h3 class="title">${arr[i].title}</h3>
+        <div class="author">${arr[i].author}</div>
+        <div class="pages">${arr[i].pages}</div>
+        <div class="year">Published in ${arr[i].year}</div>
+        <div class="read">${arr[i].read}</div>`;
+        libGrid.appendChild(newBook);
+    }
+    return arr;
+}
+displayBooks(library);
