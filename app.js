5108ec181193ed43de9e9dcc818866006014099c
Grant Collins <grantcollins03@gmail.com>
2023-04-05 08:19:15 -0400

    Form functions

--- app.js
+++ app.js
@@ -50,11 +50,13 @@ let library = [
     },
 ];
 
-class Book {
-    constructor(title, author, pages, year, read) {
-
-    }
-};
+function Book(title, author, pages, year, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.year = year,
+    this.read = read;
+}
 
 function addBookToLibrary(book) {
     return library.push(book);
@@ -68,18 +70,52 @@ function displayBooks(arr) {
         newBook.classList.add('book')
         newBook.innerHTML = `
         <h3 class="title">${arr[i].title}</h3>
-        <div class="author">${arr[i].author}</div>
-        <div class="pages">${arr[i].pages}</div>
+        <div class="author">By ${arr[i].author}</div>
+        <div class="pages">${arr[i].pages} pages</div>
         <div class="year">Published in ${arr[i].year}</div>
-        <div class="read">${arr[i].read}</div>`;
+        <div class="read">Read Status: ${arr[i].read}</div>`;
         libGrid.appendChild(newBook);
     }
     return arr;
 }
+displayBooks(library);
 
+// Show form on add btn click
 const addBtn = document.getElementById('add-btn');
+const formContainer = document.getElementById('form-container');
+const mainContainer = document.getElementById('main-container');
+
 addBtn.addEventListener('click', displayForm);
 
 function displayForm() {
-    return console.log("btn clicked!");
+    mainContainer.classList.add('blur');
+    formContainer.classList.add('active');
+}
+
+// Close form on x btn click
+const xBtn = document.getElementById('x-btn');
+xBtn.addEventListener('click', closeForm);
+
+function closeForm() {
+    mainContainer.classList.remove('blur');
+    formContainer.classList.remove('active');
+}
+
+// Get values from form
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const year = document.getElementById('year');
+const haveRead = document.getElementById('have-read');
+const notRead = document.getElementById('not-read');
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', submitBook);
+
+function submitBook(e) {
+    let newBook = new Book(title.value, author.value, pages.value, year.value, haveRead.value);
+    addBookToLibrary(newBook);
+    displayBooks(library);
+    closeForm();
+    e.preventDefault();
 }
