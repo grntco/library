ca9929815f652298e7803fffe8b2d96413a2a7e1
Grant Collins <grantcollins03@gmail.com>
2023-04-17 12:06:01 -0400

    Added position attr to new books

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 let library = [
-    {
-        title: 'Dune',
-        author: 'Frank Herbert',
-        pages: 412,
-        year: 1965,
-        read: 'Yes'
-    },
-    {
-        title: 'Foundation',
-        author: 'Isaac Asimov',
-        pages: 255,
-        year: 1951,
-        read: 'Yes'
-    },
-    {
-        title: 'The Three-Body Problem',
-        author: 'Liu Cixin',
-        pages: 302,
-        year: 2014,
-        read: 'No'
-    },
-    {
-        title: 'Project Hail Mary',
-        author: 'Andy Weir',
-        pages: 496,
-        year: 2021,
-        read: 'Yes'
-    },
-    {
-        title: 'East of Eden',
-        author: 'John Steinbeck',
-        pages: 704,
-        year: 1952,
-        read: 'Yes'
-    },
-    {
-        title: 'The Martian',
-        author: 'Andy Weir',
-        pages: 369,
-        year: 2011,
-        read: 'No'
-    },
-    {
-        title: 'Stories of Your Life and Others',
-        author: 'Ted Chiang',
-        pages: 285,
-        year: 2002,
-        read: 'Yes'
-    },
+    // {
+    //     title: 'Dune',
+    //     author: 'Frank Herbert',
+    //     pages: 412,
+    //     year: 1965,
+    //     read: 'Yes'
+    // },
+    // {
+    //     title: 'Foundation',
+    //     author: 'Isaac Asimov',
+    //     pages: 255,
+    //     year: 1951,
+    //     read: 'Yes'
+    // },
+    // {
+    //     title: 'The Three-Body Problem',
+    //     author: 'Liu Cixin',
+    //     pages: 302,
+    //     year: 2014,
+    //     read: 'No'
+    // },
+    // {
+    //     title: 'Project Hail Mary',
+    //     author: 'Andy Weir',
+    //     pages: 496,
+    //     year: 2021,
+    //     read: 'Yes'
+    // },
+    // {
+    //     title: 'East of Eden',
+    //     author: 'John Steinbeck',
+    //     pages: 704,
+    //     year: 1952,
+    //     read: 'Yes'
+    // },
+    // {
+    //     title: 'The Martian',
+    //     author: 'Andy Weir',
+    //     pages: 369,
+    //     year: 2011,
+    //     read: 'No'
+    // },
+    // {
+    //     title: 'Stories of Your Life and Others',
+    //     author: 'Ted Chiang',
+    //     pages: 285,
+    //     year: 2002,
+    //     read: 'Yes'
+    // },
 ];
 
 function Book(title, author, pages, year, read) {
@@ -147,6 +147,7 @@ function submitBook(e) {
     }
     let newBook = new Book(values[0], values[1], values[2], values[3], values[4]);
     if (checkBook(newBook)) {
+        newBook.position = library.length;
         addBookToLibrary(newBook);
         closeForm();
         displayBooks(library);
